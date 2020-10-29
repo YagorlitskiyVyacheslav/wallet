d74ef34681411e2de35bac25c41df833f9c37887
Vyacheslav <slavasan05@gmail.com>
2020-10-29 13:24:20 +0200

    header section for mobile + glogal style

--- src/component/Header/header.js
+++ src/component/Header/header.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-
+import logo from './Image/logo.svg';
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
     render() {
         return (
-            <header>
-                <div>logo
-                    <div>name logo</div>
+          <header className={styles.header}>
+            <div className={styles.container}>
+              <Link to='/home' className={styles.logoContainer}>
+                <div className={styles.imgContainer}>
+                  <img src={logo} alt="logo" />
                 </div>
-                <div>
-                    <p>Name</p>
-                    <button type='button'>Logout</button>
-                </div>
-            </header>
-        )
+                <p className={styles.logoName}>Wallet</p>
+              </Link>
+              <div className={styles.logoutContainer}>
+                <p className={styles.name}>Name</p>
+                <button type="button" className={styles.button}>
+                  Logout
+                </button>
+              </div>
+            </div>
+          </header>
+        );
     }
 }
