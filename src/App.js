ce3fc603ed36fb194275d27356722a4e73fbb54d
Serhii777 <muzykasv72@gmail.com>
2020-10-30 02:45:27 +0200

    Added Home

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Header from "./component/Header/header";
 import Layout from "./component/Layout/layout";
-import CurrencyExchange from "./component/CurrencyExchange/CurrencyExchange";
+import CurrencyExchange from "./component/CurrencyExchange";
 import NavSection from './component/NavSection/NavSection';
+import Home from './component/Home'
+import Stats from './component/Stats'
 
 class App extends Component {
   render() {
@@ -12,8 +14,8 @@ class App extends Component {
         <Header />
         <NavSection />
         <Switch>
-          <Route path="/home" />
-          <Route path="/stats" />
+          <Route path="/home" exact component={Home}/>
+          <Route path="/stats" component={Stats}/>
           <Route path="/currencies" component={CurrencyExchange} />
           <Route path="/login" />
           <Route path="/registration" />
