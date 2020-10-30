099ab64a5d549e51999732475cb9a41f8670f6ec
Vyacheslav <slavasan05@gmail.com>
2020-10-30 20:35:56 +0200

    clear master branch

--- src/component/Layout/layout.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from '../../globalStyles.module.css'
-
-const Layout = (props) => {
-    return <div className={styles.gridContainer}>{props.children}</div>;
-}
-
-export default Layout;
