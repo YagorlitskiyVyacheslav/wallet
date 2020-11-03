f125c64a3548ae1ec7947a02cf71a0be6f781ba9
unknown <artemvnua@gmail.com>
2020-11-03 03:06:17 +0300

    login mobile and tablet style

--- src/views/SignIn/SignIn.js
+++ src/views/SignIn/SignIn.js
@@ -1,38 +1,47 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import style from './SignIn.module.css';
+import walletIcon from '../../images/Authentication/walletIcon.png';
 class SignIn extends Component {
-    render() {
+  render() {
     return (
-      
-        <div>
-          <div>
-            <img src="" alt="Wallet"/>
+      <div className={style.modal}>
+        <div className={style.formContainer}>
+          <div className={style.signInLogoContainer}>
+            <img src={walletIcon} alt="Wallet" className={style.signInLogo} />
             <h1>Wallet</h1>
           </div>
 
-          <form>
+          <form className={style.formSignIn}>
             <input
               type="email"
               name="email"
               placeholder="E-mail"
+              className={style.signInEmail}
             />
             <input
               type="password"
               name="password"
-              placeholder="Password*"
+              placeholder="Пароль"
+              className={style.signInPassword}
             />
-            <button type="submit">
-              Sign in
+            <button
+              type="submit"
+              className={style.signInButton}>
+              Войти
             </button>
           </form>
-          <Link to="/registration" >
-            <h2 type="button">
-              Sign up
-            </h2>
+          <Link to="/registration" className={style.link}>
+            <p
+              type="button"
+              className={style.linkSignUp}>
+              Регистрация
+            </p>
           </Link>
         </div>
-
+      </div>
     );
-    }; };
+  }
+}
 
 export default SignIn;
