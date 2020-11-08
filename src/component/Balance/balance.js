3913d6d2fe3195fec3c17300c6c58dc7f8c7cbc8
Vyacheslav <slavasan05@gmail.com>
2020-11-08 20:28:25 +0200

    connect balance + name

--- src/component/Balance/balance.js
+++ src/component/Balance/balance.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import styles from '../NavSection/NavSection.module.css';
 
 
-export default function Balance() {
+const Balance = ({balance}) => {
     return (
       <div className={styles.balanceContainer}>
-        <p className={styles.balanceTitle}>Balance</p>
-        <p className={styles.balanceSum}>100000.00 UAN</p>
+        <p className={styles.balanceTitle}>Balance, UAN</p>
+        <p className={styles.balanceSum}>{balance}</p>
       </div>
     );
 }
+
+const mapDispatchToProps = (state) => ({
+  balance: state.transactions.balance
+})
+
+export default connect(mapDispatchToProps)(Balance);
