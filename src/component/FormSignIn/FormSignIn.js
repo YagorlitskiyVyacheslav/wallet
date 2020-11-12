109ead71ff7d1a9ed4ce8b8cee1bbd3f6c3b0bdf
unknown <artemvnua@gmail.com>
2020-11-12 18:15:52 +0300

    change authorization

---
+++ src/component/FormSignIn/FormSignIn.js
@@ -0,0 +1,82 @@
+import React, {useState} from 'react';
+import {useDispatch} from 'react-redux';
+import {Link} from 'react-router-dom';
+import {useForm} from 'react-hook-form';
+import {requestSingIn} from '../../redux/auth/authOperations';
+import walletIcon from '../../images/Authentication/walletIcon.png';
+import iPhoneIMG from '../../images/Authentication/iPhone-login-desktop.png';
+import style from './SignIn.module.css';
+
+const SignIn = () => {
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const {register, handleSubmit, errors} = useForm();
+
+  const onSubmit = () => {
+    dispatch(requestSingIn({email, password}));
+  };
+
+  return (
+    <div className={style.modal}>
+      <div className={style.iPhoneContainer}>
+        <img src={iPhoneIMG} alt="iPhone" className={style.iPhoneIMG}></img>
+        <p className={style.financeApp}>Finance App</p>
+      </div>
+      <div className={style.formContainer}>
+        <div className={style.authenticationLogoContainer}>
+          <img
+            src={walletIcon}
+            alt="Wallet"
+            className={style.authenticationLogo}
+          />
+          <h1 className={style.authenticationLogoName}>Wallet</h1>
+        </div>
+
+        <form
+          className={style.authenticationForm}
+          onSubmit={handleSubmit(onSubmit)}>
+          {
+            <label className={style.label}>
+              <input
+                type="email"
+                name="email"
+                placeholder="E-mail"
+                onChange={(event) => setEmail(event.target.value)}
+                className={`${style.authenticationEmail} ${style.inputValidation}`}
+                value={email}
+                ref={register({required: true})}
+              />
+              {errors.email && (
+                <p className={style.authenticationError}>Введите E-mail.</p>
+              )}
+            </label>
+          }
+          <label className={style.label}>
+            <input
+              value={password}
+              type="password"
+              name="password"
+              placeholder="Пароль"
+              onChange={(event) => setPassword(event.target.value)}
+              className={style.authenticationPassword}
+              ref={register({required: true})}
+            />
+            {errors.password && (
+              <p className={style.authenticationError}>Введите пароль.</p>
+            )}
+          </label>
+          <button type="submit" className={style.authenticationButton}>
+            Войти
+          </button>
+        </form>
+        <Link to="/registration" className={style.link}>
+          <p className={style.linkSignUp}>Регистрация</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
--- src/views/SignIn/SignIn.js
+++ src/views/SignIn/SignIn.js
@@ -1,83 +1,8 @@
-import React, {useState} from 'react';
-import {useDispatch} from 'react-redux';
-import {Link} from 'react-router-dom';
-import {useForm} from 'react-hook-form';
-import {requestSingIn} from '../../redux/auth/authOperations';
-import walletIcon from '../../images/Authentication/walletIcon.png';
-import iPhoneIMG from '../../images/Authentication/iPhone-login-desktop.png';
-import css from './Authentication.module.css';
-import style from './SignIn.module.css';
+import React from "react";
+import FormSignIn from "../../component/FormSignIn/FormSignIn";
 
-const SignIn = () => {
-  const dispatch = useDispatch();
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const {register, handleSubmit, errors} = useForm();
-
-  const onSubmit = () => {
-    dispatch(requestSingIn({email, password}));
-  };
-
-  return (
-    <div className={style.modal}>
-      <div className={css.iPhoneContainer}>
-        <img src={iPhoneIMG} alt="iPhone" className={css.iPhoneIMG}></img>
-        <p className={css.financeApp}>Finance App</p>
-      </div>
-      <div className={css.formContainer}>
-        <div className={css.authenticationLogoContainer}>
-          <img
-            src={walletIcon}
-            alt="Wallet"
-            className={css.authenticationLogo}
-          />
-          <h1 className={css.authenticationLogoName}>Wallet</h1>
-        </div>
-
-        <form
-          className={css.authenticationForm}
-          onSubmit={handleSubmit(onSubmit)}>
-          {
-            <label className={style.label}>
-              <input
-                type="email"
-                name="email"
-                placeholder="E-mail"
-                onChange={(event) => setEmail(event.target.value)}
-                className={`${css.authenticationEmail} ${css.inputValidation}`}
-                value={email}
-                ref={register({required: true})}
-              />
-              {errors.email && (
-                <p className={css.authenticationError}>Введите E-mail.</p>
-              )}
-            </label>
-          }
-          <label className={style.label}>
-            <input
-              value={password}
-              type="password"
-              name="password"
-              placeholder="Пароль"
-              onChange={(event) => setPassword(event.target.value)}
-              className={css.authenticationPassword}
-              ref={register({required: true})}
-            />
-            {errors.password && (
-              <p className={css.authenticationError}>Введите пароль.</p>
-            )}
-          </label>
-          <button type="submit" className={css.authenticationButton}>
-            Войти
-          </button>
-        </form>
-        <Link to="/registration" className={css.link}>
-          <p className={style.linkSignUp}>Регистрация</p>
-        </Link>
-      </div>
-    </div>
-  );
-};
+const SignIn = () => (
+    <FormSignIn />
+);
 
 export default SignIn;
