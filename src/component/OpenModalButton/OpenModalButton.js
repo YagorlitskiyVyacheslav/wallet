af117ec3882c754c3cf2948e5061dde3183f1697
RomanPidlisnychyi <myLuckyhelper@gmail.com>
2020-11-16 06:01:25 +0200

    scroll up

--- src/component/OpenModalButton/OpenModalButton.js
+++ src/component/OpenModalButton/OpenModalButton.js
@@ -1,15 +1,30 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styles from "./OpenModalButton.module.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './OpenModalButton.module.css';
 
-const OpenModalButton = ({ onToggle }) => (
-  <>
-    <div className={styles.buttonOverlay} />
-    <button className={styles.buttonOpenModal} type="button" onClick={onToggle}>
-      +
-    </button>
-  </>
-);
+const OpenModalButton = ({ onToggle }) => {
+  const onBtnClick = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+    });
+
+    onToggle();
+  };
+
+  return (
+    <>
+      <div className={styles.buttonOverlay} />
+      <button
+        className={styles.buttonOpenModal}
+        type="button"
+        onClick={onBtnClick}
+      >
+        +
+      </button>
+    </>
+  );
+};
 
 OpenModalButton.propTypes = {
   onToggle: PropTypes.func.isRequired,
